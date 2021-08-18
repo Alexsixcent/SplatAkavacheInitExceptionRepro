2b780290e4ec1e9f6a8964bf2bca7957527b6f76
Alexsixcent <alexsixcent@gmail.com>
2021-08-18 11:53:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Akavache;
+using DryIoc;
+using Splat.DryIoc;
+
+namespace SplatAkavacheInitExceptionRepro
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Registrations.Start("SplatAkavacheInitExceptionRepro");
+
+            var container = new Container();
+            try
+            {
+                container.UseDryIocDependencyResolver(); //THROWS HERE
+            }
+            catch (ContainerException)
+            {
+                Console.WriteLine("Duplicate key exception thrown !!!");
+                throw;
+            }
+        }
+    }
+}
